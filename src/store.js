4d8a8089419509fd03f8e63e35f0574bc2c60b56
zhenyafka <zhenyafka163@gmail.com>
2023-11-23 23:05:29 +0400

    Second homework

--- src/store.js
+++ src/store.js
@@ -44,7 +44,9 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {
+        code: (Math.pow(this.state.list.length, 3) / 8).toFixed(),
+        title: "Новая запись",  selectedCount: 0},]
     })
   };
 
@@ -68,7 +70,12 @@ class Store {
       ...this.state,
       list: this.state.list.map(item => {
         if (item.code === code) {
+          if (!item.selected) {
+            item.selectedCount = item.selectedCount ? item.selectedCount + 1 : 1;
+          }
           item.selected = !item.selected;
+        } else {
+          item.selected = false;
         }
         return item;
       })
